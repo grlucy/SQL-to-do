79db40d7466daad4a75cb8b69cd8c423e182dcad
unknown <53538316+grlucy@users.noreply.github.com>
2020-06-05 14:18:58 -0400

    set up server

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# SQL-to-do
-To Do List App Using React+Node+SQL
+# SQL To Do List App
+
+To Do List App Using React + Node + Express + SQL
+
+## Credits
+
+- "React + NodeJS + SQL Simple Full Stack Application" tutorial by [Theodore Anderson on YouTube](https://www.youtube.com/watch?v=HPIjjFGYSJ4)
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const routes = require("./routes");
+app.use(routes);
+
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
